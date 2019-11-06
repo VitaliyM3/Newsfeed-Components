d5f4068e3d0b2ddf3fe78c0b58ea0e16779d212c
Vitaliy Mikhaylyyk <vitaliymikhaylyuk@gmail.com>
2019-11-06 16:54:42 -0500

    5th array added

--- Article/Article.js
+++ Article/Article.js
@@ -85,6 +85,13 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: "The New Array",
+    date: "Nov 6th 2019",
+    firstParagraph: "This is the first paragraph",
+    secondParagraph: "This is the 2nd one",
+    thirdParagraph: "This is the third paragraph"
   }
 ];
 
